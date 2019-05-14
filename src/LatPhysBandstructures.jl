62c67805f58d5a43e81fe5fa80633055101dcc6a
Jan Attig <jattig@thp.Uni-Koeln.DE>
2019-05-14 15:26:04 +0200

    added bond hamiltonians to bandstructure module

--- src/LatPhysBandstructures.jl
+++ src/LatPhysBandstructures.jl
@@ -1,5 +1,48 @@
+################################################################################
+#
+#   module LatPhysBandstructures
+#   -> LatticePhysics_Base
+#   -> LatticePhysics_ReciprocalSpace
+#   -> LinearAlgebra
+#
+#   --> Definition of Bond Hamiltonians
+#   --> Definition of Hamiltonians
+#   --> Definition of Bandstructures
+#
+################################################################################
+
+
+
+
+
+
+
+
+
+
 module LatPhysBandstructures
 
-greet() = print("Hello World!")
+
+# Dependencies
+using LatPhysBase
+using LatPhysReciprocal
+using LinearAlgebra
+
+
+
+
+# BOND HAMILTONIANS
+
+# Abstract type definition
+include("bond_hamiltonian/abstract_bond_hamiltonian.jl")
+
+# Concrete Hamiltonian: Heisenberg
+include("bond_hamiltonian/concrete_bond_spin_hamiltonian_heisenberg.jl")
+# Concrete Hamiltonian: Kitaev
+include("bond_hamiltonian/concrete_bond_spin_hamiltonian_kitaev.jl")
+# Concrete Hamiltonian: Sum of two Hamiltonians
+include("bond_hamiltonian/concrete_bond_hamiltonian_sum.jl")
+
+
 
 end # module
